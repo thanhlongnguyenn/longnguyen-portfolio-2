fcce8cf8d5da95a4debfe176dfa74ace5e3c9cbb
thanhlongnguyenn <nguyenthanhlongacer@gmail.com>
2025-03-17 06:15:49 +1100

    cards

--- longnguyen-portfolio-2/src/app/components/projectcards.tsx
+++ longnguyen-portfolio-2/src/app/components/projectcards.tsx
@@ -15,7 +15,7 @@ import {
 import { cn } from "../lib/utils";
 import { AnimatePresence, motion } from "motion/react";
 import Image, { ImageProps } from "next/image";
-import { useOutsideClick } from "../hooks/use-outside-click";
+
 
 interface CarouselProps {
   items: JSX.Element[];
