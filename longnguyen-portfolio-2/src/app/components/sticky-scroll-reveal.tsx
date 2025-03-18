985910e39550ceedfd7582184f7d5e9f56740bec
thanhlongnguyenn <nguyenthanhlongacer@gmail.com>
2025-03-19 04:31:11 +1100

    Update sticky-scroll-reveal.tsx

--- longnguyen-portfolio-2/src/app/components/sticky-scroll-reveal.tsx
+++ longnguyen-portfolio-2/src/app/components/sticky-scroll-reveal.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useEffect, useRef, useState } from "react";
+import React, { useEffect, useRef } from "react";
 import { useMotionValueEvent, useScroll } from "motion/react";
 import { motion } from "motion/react";
 import { cn } from "../lib/utils";
