a5691f6c9e529d191c25ce4737a70155dd136e9f
thanhlongnguyenn <nguyenthanhlongacer@gmail.com>
2025-03-19 23:25:10 +1100

    footer

--- longnguyen-portfolio-2/src/app/sections/Footer.tsx
+++ longnguyen-portfolio-2/src/app/sections/Footer.tsx
@@ -6,7 +6,7 @@ import { ArrowUpRight } from "lucide-react";
 const Footer = () => {
 
   return (
-    <footer className="py-8 px-4 text-center mt-20">
+    <div className="py-8 px-4 text-center mt-2 relative">
       <hr className="border-t border-gray-300 mb-8" />
       <div className="mb-4 flex justify-center items-center gap-2 px-4">
         <Image src="/logo.svg" alt="Logo" width={100} height={100} />
@@ -18,28 +18,46 @@ const Footer = () => {
           <p className="font-bold">Long Nguyen</p>
           <p>Passionate learner. Innovative developer.</p>
         </div>
-        <div className="md:text-right">
-          <a
-            href="https://www.linkedin.com/in/longthanhnguyen2004/"
-            className="flex items-center gap-1"
-          >
-            LinkedIn{" "}
-            <div className="flex items-center text-[#FF611D] group-hover:translate-x-4 group-hover:-translate-y-4 transition-transform duration-300">
-              <ArrowUpRight size={20} />
-            </div>
-          </a>
-          <a
-            href="https://github.com/yourprofile"
-            className="flex items-center gap-1 mt-2"
-          >
-            GitHub{" "}
-            <div className="flex items-center text-[#FF611D] group-hover:translate-x-4 group-hover:-translate-y-4 transition-transform duration-300">
-              <ArrowUpRight size={20} />
-            </div>
-          </a>
+        <div className="md:text-right z-10 flex flex-col gap-2">
+          <div className="flex justify-between items-center group">
+            <a
+              href="https://www.linkedin.com/in/longthanhnguyen2004/"
+              className="flex-1 text-left"
+            >
+              LinkedIn
+            </a>
+            <ArrowUpRight
+              className="text-[#FF611D] transition-transform duration-300 group-hover:translate-x-1 ml-2"
+              size={20}
+            />
+          </div>
+          <div className="flex justify-between items-center group">
+            <a
+              href="https://github.com/thanhlongnguyenn"
+              className="flex-1 text-left"
+            >
+              GitHub
+            </a>
+            <ArrowUpRight
+              className="text-[#FF611D] transition-transform duration-300 group-hover:translate-x-1 ml-2"
+              size={20}
+            />
+          </div>
+          <div className="flex justify-between items-center group">
+            <a
+              href="mailto:tln.thanhlongnguyen@gmail.com?subject=Hello%20Long!%20I%20am%20a%20visitor%20from%20your%20portfolio"
+              className="flex-1 text-left"
+            >
+              Email
+            </a>
+            <ArrowUpRight
+              className="text-[#FF611D] transition-transform duration-300 group-hover:translate-x-1 ml-2"
+              size={20}
+            />
+          </div>
         </div>
       </div>
-    </footer>
+    </div>
   );
 };
 
